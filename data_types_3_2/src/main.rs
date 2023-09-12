670fc87e3333940d2be692fbbf4e50895e13dcfc
asad2200 <asadjakhavala92@gmail.com>
2023-09-12 23:55:22 +0530

    - Updated data types

--- data_types_3_2/src/main.rs
+++ data_types_3_2/src/main.rs
@@ -47,11 +47,15 @@
 
 // tup
 fn main(){
-    let a = [1, 2, 3, 4, 5];
+    // let a = [1, 2, 3, 4, 5];
     let a: [i32; 5] = [1, 2, 3, 4, 5];
 
     let b = [3; 5]; // same value(3) for 5 times
 
+    println!("b : {:?}", b);
+
     let first = a[0];
     let second = a[1];
+
+    println!("First: {first}, Second: {second}");
 }
